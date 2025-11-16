803258ac6b1069ca9fb930fea023186e34b5bf6e
Robert Drucker <88389953+rwdrucker@users.noreply.github.com>
2025-11-16 07:53:56 -0500

    Server Component Login Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { LoginForm } from "@/components/login-form";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <LoginForm />
     </div>
   );
 }
---
+++ components/login-form.tsx
@@ -0,0 +1,22 @@
+import { signIn } from "@/auth";
+
+export function LoginForm() {
+  return (
+    <form
+      action={async (formData) => {
+        "use server";
+        await signIn("credentials", formData);
+      }}
+    >
+      <label>
+        Username
+        <input name="username" type="text" />
+      </label>
+      <label>
+        Password
+        <input name="password" type="password" />
+      </label>
+      <button>Sign In</button>
+    </form>
+  );
+}
